0266f93c1c4e365494a6d2c451dd0c7c345f08e2
Cristiano Silva <alchaarcristiano@gmail.com>
2023-02-02 10:00:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# discover-14-rocket-coffee
+
+![Página rocket-coffee](https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f35a907f-c404-4e99-9de0-9e513e24df49/Kapture_2021-11-16_at_15.31.34.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230201%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230201T185154Z&X-Amz-Expires=86400&X-Amz-Signature=58e735472ffa6f3e640bcfde8784748b578e134e680cfc828cc0e7cdded941f7&X-Amz-SignedHeaders=host&x-id=GetObject)
+
+---------------
+
+**To make this code work its necessary to insert an API key from https://www.themoviedb.org/ in src/api.js **
+
+Project developed with __HTML__ ,__CSS__ and __JS__
+
+Challenge made for "Discover" / [@Rocketseat](https://github.com/Rocketseat)
+
+By [Cristiano Alchaar da Silva](https://github.com/CristianoAlchaar)
---
+++ src/script.js
@@ -0,0 +1,52 @@
+const user = "diego3g"
+const URL = "https://api.github.com/users/" + user;
+
+const profileName = document.querySelector('div#card header h2');
+const profileImg = document.querySelector('img#profile-img');
+const profileFollowers = document.querySelector('span#followers');
+const profileFollowing = document.querySelector('span#following');
+const profileRepositories = document.querySelector('span#repositories');
+const profileCompany = document.querySelector('span#company');
+const profileLocation = document.querySelector('span#location');
+const generateBackgroundButton = document.getElementById('btn-generate-background');
+
+async function getProfileData(urlProfile) {
+    try {
+      const response = await axios.get(urlProfile);
+      return(response.data) 
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+async function loadProfileDataOnScreen(urlProfile) {
+    const profileData = await getProfileData(urlProfile);
+    console.log(profileData);
+    profileName.innerHTML = profileData.login;
+    profileImg.src = profileData.avatar_url;
+    profileFollowers.innerHTML = profileData.followers + " seguidores";
+    profileFollowing.innerHTML = profileData.following + " seguindo";
+    profileRepositories.innerHTML = profileData.public_repos + " repositórios";
+    profileCompany.innerHTML = profileData.company;
+    profileLocation.innerHTML = profileData.location;
+}
+
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
+
+function changeBackground(){
+    const cardProfile = document.getElementById("card");
+    let randomColor = generateRandomColor();
+    cardProfile.style.borderColor = randomColor;
+}
+
+
+loadProfileDataOnScreen(URL);
+
+generateBackgroundButton.addEventListener("click", () => {changeBackground()});
